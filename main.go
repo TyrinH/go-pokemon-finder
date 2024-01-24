3314e448ec07d2e3216d54787c8b79671d495497
Tyrin Holloway <tychnk@gmail.com>
2024-01-23 18:42:00 -0600

    added in initial template and included html template package to begin serving pokemon api responses

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,10 +9,43 @@ import (
 	"github.com/mtslzr/pokeapi-go"
 )
 
+type Pokemon struct {
+	Name string
+	Image string
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/", func (c *gin.Context)  {
-		
+		tmpl := template.Must(template.ParseFiles("./index.html"))
+		bulbasaurResponse, _ := pokeapi.Pokemon("bulbasaur")
+		charmanderResponse, _ := pokeapi.Pokemon("charmander")
+		squirtleResponse, _ := pokeapi.Pokemon("squirtle")
+
+		bulbasaur := Pokemon{
+			Name: bulbasaurResponse.Name,
+			Image: bulbasaurResponse.Sprites.FrontDefault,
+		}
+		charmander := Pokemon{
+			Name: charmanderResponse.Name,
+			Image: charmanderResponse.Sprites.FrontDefault,
+
+		}
+		squirtle := Pokemon{
+			Name: squirtleResponse.Name,
+			Image: squirtleResponse.Sprites.FrontDefault,
+
+		}
+
+		starterPokemon := map[string][]Pokemon{
+			"Pokemons": {
+				bulbasaur,
+				charmander,
+				squirtle,
+				
+			},
+		}
+		tmpl.Execute(c.Writer, starterPokemon)
 	})
 	r.GET("/ping", func(c *gin.Context)  {
 		pokemon, _ := pokeapi.Pokemon("pikachu")
